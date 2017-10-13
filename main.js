497baae3f1480003b5c89b6a361e81b41291d65a
Flavins <maduabuchiokonkwo@gmail.com>
2017-10-13 02:06:49 +0100

    nav bar responsive complete

---
+++ main.js
@@ -0,0 +1,15 @@
+
+            function dropdownMenu(){
+                var x = document.getElementById("dropdownClick");
+                if(x.className === "topnav"){
+                    x.classsName += " responsive";
+                    
+                /** change topnav to topnav.responsve**/
+
+                }else{
+                x.className = "topnav";
+                } 
+            
+
+            }
+                
